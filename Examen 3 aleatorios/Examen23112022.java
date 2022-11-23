22b7976e189509848ad99e445a95071609de1af7
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-11-23 11:12:56 +0100

    feat: examen23112022 funcionando todo el ciclo

--- Examen 3 aleatorios/Examen23112022.java
+++ Examen 3 aleatorios/Examen23112022.java
@@ -12,32 +12,36 @@ import java.util.Scanner;
 public class Examen23112022 {
     public static void main(String[] args) {
         // Var declarations
-        String play;
+        int play;
         int minPlayerInterval;
         int maxPlayerInterval;
         int minInterval;
         int maxInterval;
+        int intentos;
         int num;
-
-        // Var init
-        play = "";
+        int numFinal;
+        boolean win;
 
         // Scanner class
         Scanner sc = new Scanner(System.in);
 
         System.out.println("-A-L-E-A-T-O-R-I-U-M-");
         System.out.println("Aleatorium pensará un número entre los valores que tu decidas");
+
+        // Solicitamos el rango al usuario
+        System.out.print("\nIntroduzca el mínimo valor del intervalo: ");
+        minPlayerInterval = sc.nextInt();
         do {
-            // Solicitamos el rango al usuario
-            System.out.print("Introduzca el mínimo valor del intervalo: ");
-            minPlayerInterval = sc.nextInt();
-            do {
-                System.out.print("Introduzca el máximo valor del intervalo: ");
-                maxPlayerInterval = sc.nextInt();
-                if (maxPlayerInterval <= minPlayerInterval)
-                    System.out.println(maxPlayerInterval
-                            + " no es un número de intervalo correcto, debe ser mayor que  " + minPlayerInterval);
-            } while (maxPlayerInterval <= minPlayerInterval);
+            System.out.print("Introduzca el máximo valor del intervalo: ");
+            maxPlayerInterval = sc.nextInt();
+            if (maxPlayerInterval <= minPlayerInterval)
+                System.out.println(maxPlayerInterval
+                        + " no es un número de intervalo correcto, debe ser mayor que  " + minPlayerInterval);
+        } while (maxPlayerInterval <= minPlayerInterval);
+
+        // Bucle general del proceso de juego
+        do {
+            win = false;
 
             // Generamos el nuevo rango interior
             do {
@@ -47,13 +51,46 @@ public class Examen23112022 {
 
             // Mostramos el intervalo
             System.out.println("-A-L-E-A-T-O-R-I-U-M-");
-            System.out.printf("Estoy pensando en un número entre %d y %d", minInterval, maxInterval);
+            System.out.printf("Estoy pensando en un número entre %d y %d\n", minInterval, maxInterval);
+
+            // Calculamos el número a buscar y el número de intentos y lo mostramos
+            numFinal = (int) (Math.random() * ((maxInterval - minInterval) + 1)) + minInterval;
+            intentos = (int) (Math.log(maxInterval - minInterval + 1) / Math.log(2));
+            System.out.println("Tienes " + intentos + " intentos");
+
+            // Juego del jugador
+            do {
+                System.out.print("Escribe el número: ");
+                num = sc.nextInt();
+                if (num > numFinal)
+                    System.out.println("Te has pasado");
+                else if (num < numFinal)
+                    System.out.println("Te has quedado corto");
+                else
+                    win = true;
+            } while (--intentos > 0 && !win);
+
+            // Mostramos mensaje de victoria o derrota
+            System.out.println();
+            if (win) {
+                System.out.println("Enhorabuena!!!! has acertado.");
+                System.out.printf("Efectivamente, el número era: %d\n", numFinal);
+            } else {
+                System.out.println("Has superado el número de intentos");
+                System.out.printf("El número que había pensado era: %d\n", numFinal);
+            }
 
             // Preguntamos si quiere seguir jugando
-            System.out.print("\n¿Quiéres seguir jugando (s para continuar):");
-            sc.nextLine();
-            play = sc.next();
-        } while (play.equals("s") || play.equals("S"));
+            System.out.println("\nElige una opción");
+            System.out.println("1. Volver a jugar");
+            System.out.println("2. Salir del juego");
+            do {
+                play = sc.nextInt();
+                if (play != 1 && play != 2)
+                    System.out.print("Por favor, introduce una opción correcta (1/2): ");
+            } while (play != 1 && play != 2);
+        } while (play != 2);
+
         // Close scanner
         sc.close();
     }
