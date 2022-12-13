e736e235c680ec363633ed14be6d690c3a8cb5d6
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-12-13 13:48:00 +0100

    fix: multiplicación

--- Examen 5 funciones/Examen13122022.java
+++ Examen 5 funciones/Examen13122022.java
@@ -2,7 +2,7 @@ import matematicas.Operaciones;
 
 public class Examen13122022 {
     public static void main(String[] args) {
-        System.out.println("  Pruebas unitarias");
+ /*        System.out.println("  Pruebas unitarias");
         System.out.println("**********************");
         System.out.printf("Función suma: %s\n", Operaciones.suma(5,3)==8 && Operaciones.suma(-5,-3)==-8?"correcto":"fallo");
         System.out.printf("Función inc: %s\n", Operaciones.inc(5)==6 && Operaciones.inc(-5)==-4?"correcto":"fallo");
@@ -21,29 +21,14 @@ public class Examen13122022 {
         System.out.printf("Función esDivisor: %s\n", Operaciones.esDivisor(5, 25) && !Operaciones.esDivisor(3, 25)?"correcto":"falso");
         System.out.printf("Función esPrimo: %s\n", Operaciones.esPrimo(13) && !Operaciones.esPrimo(4) && !Operaciones.esPrimo(-5) && !Operaciones.esPrimo(1) && !Operaciones.esPrimo(0)?"correcto":"falso");
         System.out.printf("Función esCuadradoPerfecto: %s\n", Operaciones.esCuadradoPerfecto(25) && !Operaciones.esCuadradoPerfecto(8)?"correcto":"falso");
-
+ */
 
 
     System.out.println("\n**********************");
     System.out.println("  Pruebas unitarias");
     System.out.println("**********************");
-    System.out.printf("Función suma: %s\n", Operaciones.suma((long) 5,(long) 3)==8 && Operaciones.suma((long)-5,(long)-3)==-8?"correcto":"fallo");
-    System.out.printf("Función inc: %s\n", Operaciones.inc((long)5)==6 && Operaciones.inc((long)-5)==-4?"correcto":"fallo");
-    System.out.printf("Función resta: %s\n", Operaciones.resta((long)5,(long)3)==2 && Operaciones.resta((long)-5,(long) -3)==-2?"correcto":"fallo");
-    System.out.printf("Función dec: %s\n", Operaciones.dec((long)5)==4 && Operaciones.dec((long)-5)==-6?"correcto":"fallo");
-    System.out.printf("Función esMayor: %s\n", Operaciones.esMayor((long)5,(long)4) && !Operaciones.esMayor((long)4, (long)5) && !Operaciones.esMayor((long)5,(long)5)?"correcto":"falso");
-    System.out.printf("Función esMenor: %s\n", Operaciones.esMenor((long)4,(long)5) && !Operaciones.esMenor((long)5, (long)4) && !Operaciones.esMenor((long)5,(long)5)?"correcto":"falso");
-    System.out.printf("Función esIgual: %s\n", Operaciones.esIgual((long)5,(long)5)?"correcto":"falso");
-    System.out.printf("Función multiplica: %s\n", Operaciones.multiplica((long)5,(long)2)==10?"correcto":"falso");
-        System.out.printf("Función divide: %s\n", Operaciones.divide((long)10,(long)2)==5?"correcto":"falso");
-        System.out.printf("Función resto: %s\n", Operaciones.resto((long)10,(long)3)==1 && Operaciones.resto((long)10, (long)2)==0?"correcto":"falso");
-        System.out.printf("Función potencia: %s\n", Operaciones.potencia((long)2,(long)3)==8?"correcto":"falso");
-        System.out.printf("Función cuadrado: %s\n", Operaciones.cuadrado((long)3)==9?"correcto":"falso");
-        System.out.printf("Función cubo: %s\n", Operaciones.cubo((long)3)==27?"correcto":"falso");
-        System.out.printf("Función esMultiplo: %s\n", Operaciones.esMultiplo((long)25, (long)5) && !Operaciones.esMultiplo((long)25, (long)3)?"correcto":"falso");
-        System.out.printf("Función esDivisor: %s\n", Operaciones.esDivisor((long)5, (long)25) && !Operaciones.esDivisor((long)3, (long)25)?"correcto":"falso");
-        System.out.printf("Función esPrimo: %s\n", Operaciones.esPrimo((long)13) && !Operaciones.esPrimo((long)4) && !Operaciones.esPrimo((long)-5) && !Operaciones.esPrimo((long)1) && !Operaciones.esPrimo((long)0)?"correcto":"falso");
-        System.out.printf("Función esCuadradoPerfecto: %s\n", Operaciones.esCuadradoPerfecto((long)25) && !Operaciones.esCuadradoPerfecto((long)8)?"correcto":"falso");
-
+    int a= 10;
+    int b= 5;
+    System.out.printf("Función: %s\n", Operaciones.multiplica(a,b));
 }
 }
--- Examen 5 funciones/matematicas/Operaciones.java
+++ Examen 5 funciones/matematicas/Operaciones.java
@@ -3,201 +3,216 @@ package matematicas;
 public class Operaciones {
 
     /**
-     * suma: Realiza la suma de dos valores
-     *
-     * @param a Sumando 1
-     * @param b Sumando 2
-     * @return long
+     * suma
+     * Devuelve la suma de dos números.
+     * 
+     * @param a Sumando primero
+     * @param b Sumando segundo
+     * @return La suma de a y b
      */
     public static long suma(long a, long b) {
         return (a + b);
     }
 
     /**
-     * suma: Realiza la suma de dos valores
-     *
-     * @param a Sumando 1
-     * @param b Sumando 2
-     * @return int
+     * suma
+     * Devuelve la suma de dos números.
+     * 
+     * @param a Sumando primero
+     * @param b Sumando segundo
+     * @return La suma de a y b
      */
     public static int suma(int a, int b) {
         return ((int) suma((long) a, (long) b));
     }
 
     /**
-     * inc: Incrementa el valor de un valor en uno
-     *
-     * @param a Número a incrementar
-     * @return long
+     * inc
+     * Incrementa en uno el valor recibido y lo devuelve
+     * 
+     * @param a Valor a incrementar
+     * @return El valor recibido incrementado en uno
      */
     public static long inc(long a) {
         return (suma(a, 1));
     }
 
     /**
-     * inc: Incrementa el valor de un valor en uno
-     *
-     * @param a Número a incrementar
-     * @return int
+     * inc
+     * Incrementa en uno el valor recibido y lo devuelve
+     * 
+     * @param a Valor a incrementar
+     * @return El valor recibido incrementado en uno
      */
     public static int inc(int a) {
         return ((int) inc((long) a));
     }
 
     /**
-     * resta: Realiza la resta de dos número, a - b
-     *
-     * @param a Número 1
-     * @param b Número 2
-     * @return long
+     * resta
+     * Devuelve la resta de dos números
+     * 
+     * @param a Minuendo
+     * @param b Sustraendo
+     * @return La resta de los dos números
      */
     public static long resta(long a, long b) {
         return (suma(a, -b));
     }
 
     /**
-     * resta: Realiza la resta de dos número, a - b
-     *
-     * @param a Número 1
-     * @param b Número 2
-     * @return int
+     * resta
+     * Devuelve la resta de dos números
+     * 
+     * @param a Minuendo
+     * @param b Sustraendo
+     * @return La resta de los dos números
      */
     public static int resta(int a, int b) {
         return ((int) resta((long) a, (long) b));
     }
 
     /**
-     * dec: Decrece el número pasado por parámetro en 1
-     *
-     * @param a Número a decrecer
-     * @return long
+     * dec
+     * Decrementa en uno el valor recibido y lo devuelve
+     * 
+     * @param a Valor a decrementar
+     * @return El valor recibido decrementado en uno
      */
     public static long dec(long a) {
         return (resta(a, 1));
     }
 
     /**
-     * dec: Decrece el número pasado por parámetro en 1
-     *
-     * @param a Número a decrecer
-     * @return long
+     * dec
+     * Decrementa en uno el valor recibido y lo devuelve
+     * 
+     * @param a Valor a decrementar
+     * @return El valor recibido decrementado en uno
      */
     public static int dec(int a) {
         return ((int) dec((long) a));
     }
 
     /**
-     * esMayor: Compara dos números y dice si el primer parámetro es mayor que el
-     * segundo
-     *
-     * @param a Número 1 a comparar
-     * @param b Número 2 a comparar
-     * @return boolean
+     * esMayor
+     * Comprueba si un número es mayor que otro
+     * 
+     * @param a Valor a comparar si es mayor
+     * @param b Valor a comparar si es menor
+     * @return true si a es mayor que b y
+     *         false si es igual o menor
      */
     public static boolean esMayor(long a, long b) {
         return (resta(a, b) > 0 ? true : false);
     }
 
     /**
-     * esMayor: Compara dos números y dice si el primer parámetro es mayor que el
-     * segundo
-     *
-     * @param a Número 1 a comparar
-     * @param b Número 2 a comparar
-     * @return boolean
+     * esMayor
+     * Comprueba si un número es mayor que otro
+     * 
+     * @param a Valor a comparar si es mayor
+     * @param b Valor a comparar si es menor
+     * @return true si a es mayor que b y
+     *         false si es igual o menor
      */
     public static boolean esMayor(int a, int b) {
         return (esMayor((long) a, (long) b));
     }
 
     /**
-     * esMenor: Compara dos números y dice si el primer parámetro es menor que el
-     * segundo
-     *
-     * @param a Número 1 a comparar
-     * @param b Número 2 a comparar
-     * @return boolean
+     * esMenor
+     * Comprueba si un número es menor que otro
+     * 
+     * @param a Valor a comparar si es menor
+     * @param b Valor a comparar si es mayor
+     * @return true si a es menor que b y
+     *         false si es igual o mayor
      */
     public static boolean esMenor(long a, long b) {
         return (resta(b, a) > 0 ? true : false);
     }
 
     /**
-     * esMenor: Compara dos números y dice si el primer parámetro es menor que el
-     * segundo
-     *
-     * @param a Número 1 a comparar
-     * @param b Número 2 a comparar
-     * @return boolean
+     * esMenor
+     * Comprueba si un número es menor que otro
+     * 
+     * @param a Valor a comparar si es menor
+     * @param b Valor a comparar si es mayor
+     * @return true si a es menor que b y
+     *         false si es igual o mayor
      */
     public static boolean esMenor(int a, int b) {
         return ((int) resta((long) b, (long) a) > 0 ? true : false);
     }
 
     /**
-     * esIgual: Compara si dos números son iguales
-     *
-     * @param a Número 1 a comparar
-     * @param b Número 2 a comparar
-     * @return boolean
+     * esIgual
+     * Comprueba si un número es igual que otro
+     * 
+     * @param a Primer valor a comparar
+     * @param b Segundo valor a comparar
+     * @return true si son iguales y false si son distintos
      */
     public static boolean esIgual(long a, long b) {
         return (!esMayor(a, b) && !esMenor(a, b) ? true : false);
     }
 
     /**
-     * esIgual: Compara si dos números son iguales
-     *
-     * @param a Número 1 a comparar
-     * @param b Número 2 a comparar
-     * @return boolean
+     * esIgual
+     * Comprueba si un número es igual que otro
+     * 
+     * @param a Primer valor a comparar
+     * @param b Segundo valor a comparar
+     * @return true si son iguales y false si son distintos
      */
     public static boolean esIgual(int a, int b) {
         return (!esMayor((long) a, (long) b) && !esMenor((long) a, (long) b) ? true : false);
     }
 
     /**
-     * multiplica: Multiplica dos números
-     *
-     * @param a Número 1
-     * @param b Número 2
-     * @return long
+     * multiplica
+     * Devuelve la multiplicación de dos números usando sumas
+     * 
+     * @param a Multiplicando
+     * @param b Multiplicador
+     * @return El resultado de sumar b veces a
      */
     public static long multiplica(long a, long b) {
         long res = 0;
-        /*
-         * int signo = 0;
-         * if (esMenor(a, 0))
-         * signo = dec(signo);
-         * if (esMenor(b, 0))
-         */
-        /********** controlar las multiplicaciones de números negativos **********/
-
-        while (esMayor(b, 0)) {
-            res = suma(res, a);
-            b = dec(b);
+        long auxA = a;
+        long auxB = b;
+        if (esMenor(a, 0))
+            auxA = -a;
+        if (esMenor(b, 0))
+            auxB = -b;
+        while (esMayor(auxB, 0)) {
+            res = suma(res, auxA);
+            auxB = dec(auxB);
         }
-
-        return (res);
+        return ((esMenor(a, 0) && esMayor(b, 0)) || ((esMayor(a, 0) && esMenor(b, 0))) ? -res : res);
     }
 
     /**
-     * multiplica: Multiplica dos números
-     *
-     * @param a Número 1
-     * @param b Número 2
-     * @return int
+     * multiplica
+     * Devuelve la multiplicación de dos números usando sumas
+     * 
+     * @param a Multiplicando
+     * @param b Multiplicador
+     * @return El resultado de sumar b veces a
      */
     public static int multiplica(int a, int b) {
         return ((int) multiplica((long) a, (long) b));
     }
 
     /**
-     * divide: Divide dos números
-     *
-     * @param a Número 1
-     * @param b Número 2
-     * @return long
+     * divide
+     * Devuelve el cociente de la división entera de dos números usando restas
+     * 
+     * @param a Dividendo
+     * @param b Divisor
+     * @return Devuelve el cociente de la división entera de a entre b
+     *         realizando restas
      */
     public static long divide(long a, long b) {
         long res = 0;
@@ -209,11 +224,13 @@ public class Operaciones {
     }
 
     /**
-     * divide: Divide dos números
-     *
-     * @param a Número 1
-     * @param b Número 2
-     * @return int
+     * divide
+     * Devuelve el cociente de la división entera de dos números usando restas
+     * 
+     * @param a Dividendo
+     * @param b Divisor
+     * @return Devuelve el cociente de la división entera de a entre b
+     *         realizando restas
      */
     public static int divide(int a, int b) {
         return ((int) divide((long) a, (long) b));
